19ff83b6ea4085fe49e3fb5d7d9c46fc31eafe0f
Evan0306011 <abu.bashar5969@gmail.com>
2022-06-16 23:43:41 +1000

    jhhhj

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,8 +56,8 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x2d33260A2926d4E1aa6F330959bea9B0a4c812dd';
 const TREASURY_ADDRESS = '0x2d33260A2926d4E1aa6F330959bea9B0a4c812dd';
 const MAX_SUPPLY = 200; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 20; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const MINT_PRICE = 50; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-06-13T22:00:08+10:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
