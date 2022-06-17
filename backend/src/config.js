3c73e5326c2a12e45b4dfd87ff5e98c73f9fc6d5
Evan0306011 <abu.bashar5969@gmail.com>
2022-06-18 00:17:25 +1000

    hkjhkhk

--- backend/src/config.js
+++ backend/src/config.js
@@ -57,7 +57,7 @@ const OWNER_ADDRESS = '0x2d33260A2926d4E1aa6F330959bea9B0a4c812dd';
 const TREASURY_ADDRESS = '0x2d33260A2926d4E1aa6F330959bea9B0a4c812dd';
 const MAX_SUPPLY = 500; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 8; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-06-13T22:00:08+10:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
@@ -71,7 +71,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x75228B48Cd3d18690225eA387b3A9D0a799118c5"; // If you want to manually include it
+let CONTRACT_ADDRESS = ""; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
