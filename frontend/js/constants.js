ea50cc90c6986b39c2ff8893332073ae6a84ed14
Evan0306011 <abu.bashar5969@gmail.com>
2022-06-18 00:07:33 +1000

    lklk

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x8aec4bCa71af2E8fE8406bfb39Aa8c47f1589b75"; //Replace with your own contract address
+const contractAddress = "0x75228B48Cd3d18690225eA387b3A9D0a799118c5"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the AussieBloke's NFT Project!!";
