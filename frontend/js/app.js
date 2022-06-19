c859f35f794094ba5ffb2ac9ec774b2dbb00a1a9
Evan0306011 <abu.bashar5969@gmail.com>
2022-06-20 01:53:48 +1000

    dfdf

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -286,7 +286,7 @@ function setTotalPrice() {
     mintInput.disabled = true;
     return;
   }
-  const totalPriceWei = BigInt(info.deploymentConfig.mintPrice) * BigInt(mintInputValue);
+  const totalPriceWei = BigInt(info.deploymentConfig.tokensPerMint) * BigInt(mintInputValue);
   
   let priceType = '';
   if(chain === 'rinkeby') {
