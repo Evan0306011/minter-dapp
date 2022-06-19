2c60b777d20d321f6f04d4d8b292d3c2f0e46b92
Evan0306011 <abu.bashar5969@gmail.com>
2022-06-20 00:12:44 +1000

    kll

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
 const contractAddress = "0xaeC30Ccd9AC9E668FaE67621CbDb103C78CaCCf3"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
